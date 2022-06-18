c0c0dfb1879fc747cf93af97caed382c80cd92de
hoangdaibang <hoangdaibang0804@gmail.com>
2022-06-18 15:25:39 +0700

    create new project

---
+++ helloworld/addTwoInterger.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumber;
+
+    cout << "Enter two integers : ";
+    cin >> firstNumber >> secondNumber ;
+    sumOfTwoNumber = firstNumber + secondNumber ;
+    cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumber ;
+
+
+    return 0;
+}
